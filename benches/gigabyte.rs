d8a835a553477c8331b02f13065aca1c096f5b8f
Joonas Koivunen <joonas.koivunen@gmail.com>
2020-11-16 17:41:42 +0200

    refactor: use parametrized benches, throughput

--- benches/gigabyte.rs
+++ benches/gigabyte.rs
@@ -3,28 +3,47 @@ extern crate core;
 extern crate criterion;
 
 use core::hash::Hasher;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput, BenchmarkId};
 
 fn gigabyte(c: &mut Criterion) {
-    c.bench_function("buffer", |b| {
-        b.iter({
-            let buf = [15; 4096];
-            move || {
-                black_box(seahash::hash(&buf));
-            }
-        })
-    });
+    let buf = vec![15; 4 * 4096];
+    let sizes = [8, 64, 1024, 4096, 4 * 4096];
 
-    c.bench_function("gigabyte_stream", |b| b.iter(|| {
-        let mut buf = [15;4096];
-        let mut hasher = seahash::SeaHasher::default();
+    let mut group = c.benchmark_group("buffer");
 
-        for _ in 0..250_000 {
-            Hasher::write(&mut hasher,&buf);
-            buf[0] += buf[0].wrapping_add(1);
-        }
-        hasher.finish()
-    }));
+    for size in &sizes {
+        group.throughput(Throughput::Bytes(*size as u64));
+
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            b.iter(|| {
+                black_box(seahash::hash(&buf[..size]));
+            })
+        });
+    }
+
+    group.finish();
+
+    let mut group = c.benchmark_group("stream");
+
+    for size in &sizes {
+        group.throughput(Throughput::Bytes(*size as u64));
+
+        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+            b.iter_with_setup(
+                || seahash::SeaHasher::default(),
+                |mut h: seahash::SeaHasher| {
+                    // use chunks of 32 bytes to simulate some looping on a single hasher value
+                    for _ in 0..size/32 {
+                        h.write(&buf[..32]);
+                    }
+                    // this will mostly be an empty slice, but that is a possible Hasher api usage
+                    h.write(&buf[..(size % 32)]);
+                    black_box(h.finish())
+            })
+        });
+    }
+
+    group.finish();
 }
 
 criterion_group!(benches, gigabyte);
